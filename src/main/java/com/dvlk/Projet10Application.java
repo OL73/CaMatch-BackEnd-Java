a05ba97ca87223867d02d0049e56c17119b2d4c2
Olivier Lambin <by9700@yahoo.fr>
2018-06-06 16:38:11 +0200

    first commit

---
+++ src/main/java/com/dvlk/Projet10Application.java
@@ -0,0 +1,12 @@
+package com.dvlk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet10Application.class, args);
+	}
+}
---
+++ src/test/java/com/dvlk/Projet10ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dvlk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Projet10ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
