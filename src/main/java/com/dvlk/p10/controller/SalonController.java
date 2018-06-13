66fac9c25dc92c3219b9abf0a1b83462599d6549
damien <damien.diep@gmail.com>
2018-06-13 20:08:43 +0200

    push json

--- src/main/java/com/dvlk/p10/controller/SalonController.java
+++ src/main/java/com/dvlk/p10/controller/SalonController.java
@@ -1,32 +1,21 @@
 package com.dvlk.p10.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dvlk.p10.bean.Salon;
-import com.dvlk.p10.repository.SalonRepository;
-
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("/api")
 public class SalonController {
-	@Autowired
-	private SalonRepository salonRepository;
-	
-	@GetMapping("/")
-	public List<Salon> getSalons(List<Salon> lstSalons) {
-		return lstSalons;
-	}
 
-	@GetMapping("/salon/{id}")
-	public Salon getSalon(@PathVariable("id") int id, Salon salon) {
-		return salon;
+	@GetMapping("/salons")
+	public Salon getSalons() {
+		Salon s = new Salon();
+		return s;
 	}
 	
 
