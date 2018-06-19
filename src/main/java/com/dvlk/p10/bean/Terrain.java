7d2e8227901569c3ebb4df328d72d0e1652748db
damien diep <damien.diep@gmail.com>
2018-06-19 09:50:44 +0200

    terrain class added package in import

--- src/main/java/com/dvlk/p10/bean/Terrain.java
+++ src/main/java/com/dvlk/p10/bean/Terrain.java
@@ -1,9 +1,11 @@
+package com.dvlk.p10.bean;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
