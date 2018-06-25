d4f6496442ccc29e1118100dca97bb2782ecd0ec
damien diep <damien.diep@gmail.com>
2018-06-25 11:00:05 +0200

    SportService saveSport method post en Json

--- src/main/java/com/dvlk/p10/controller/SportController.java
+++ src/main/java/com/dvlk/p10/controller/SportController.java
@@ -4,6 +4,7 @@ package com.dvlk.p10.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +15,12 @@ import com.dvlk.p10.service.ISportService;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api")
 public class SportController {
-	
+
 	@Autowired
 	private ISportService service;
-	
+
 	@PostMapping("/sport")
-	public Sport saveSport(String nom, String description) {
-		Sport sport = new Sport();
-		sport.setNom(nom);
-		sport.setDescription(description);
+	public Sport saveSport(@RequestBody Sport sport) {
 		this.service.saveOne(sport);
 		return sport;
 	}
