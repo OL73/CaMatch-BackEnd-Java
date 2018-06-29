514eb8c48ebd1db45dbc1094a217d7ca6bb92b29
olivier lambin <by9700@yahoo.fr>
2018-06-29 16:51:52 +0200

    DTA lieu et terrain ont été ajoutés

---
+++ src/main/java/com/dvlk/p10/dto/LieuDTO.java
@@ -0,0 +1,11 @@
+package com.dvlk.p10.dto;
+
+public class LieuDTO {
+
+	private Integer id;
+	private String nom;
+	private String adresse;
+	private String codePostal;
+	private String numTelephone;
+
+}
---
+++ src/main/java/com/dvlk/p10/dto/TerrainDTO.java
@@ -0,0 +1,28 @@
+package com.dvlk.p10.dto;
+
+public class TerrainDTO {
+
+	private String libelle;
+	private Integer lieu_id;
+
+	public TerrainDTO() {
+
+	}
+
+	public String getLibelle() {
+		return this.libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Integer getLieu_id() {
+		return this.lieu_id;
+	}
+
+	public void setLieu_id(Integer lieu_id) {
+		this.lieu_id = lieu_id;
+	}
+
+}
