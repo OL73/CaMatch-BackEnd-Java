0d4e69126162be2f22400a85e51e684e827e5945
damien diep <damien.diep@gmail.com>
2018-07-03 16:36:37 +0200

    Spring security ajouté et encodage Bcrypt ajouté a linscription utilisateur

--- src/main/java/com/dvlk/p10/controller/UtilisateurController.java
+++ src/main/java/com/dvlk/p10/controller/UtilisateurController.java
@@ -3,6 +3,8 @@ package com.dvlk.p10.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,14 +29,14 @@ public class UtilisateurController {
 	private PasswordEncoder passwordEncoder;
 
 	@PutMapping("/inscription")
-	public UtilisateurDTO creationUtilisateur(@RequestBody Utilisateur utilisateur) {
+	public ResponseEntity<Object> creationUtilisateur(@RequestBody Utilisateur utilisateur) {
 		UtilisateurController.LOG.info("Dans la méthode POST UtilisateurController, PSEUDO : {} ,DATE : {}",
 				utilisateur.getPseudo(), utilisateur.getDateNaissance());
 		UtilisateurDTO utilisateurDTO = new UtilisateurDTO(utilisateur);
 		utilisateurDTO.setEmail(utilisateur.getEmail());
-		utilisateurDTO.setPassword(passwordEncoder.encode(utilisateur.getPassword()));
+		utilisateur.setPassword(passwordEncoder.encode(utilisateur.getPassword()));
 		this.service.saveOne(utilisateur);
-		return utilisateurDTO;
+		return new ResponseEntity<Object>(utilisateurDTO, HttpStatus.ACCEPTED);
 	}
 
 }
