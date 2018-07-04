ec5285049c22ebc55fb8a06ed5f201914817fec5
damien diep <damien.diep@gmail.com>
2018-07-04 11:40:05 +0200

    Spring security permit

--- src/main/java/com/dvlk/SpringSecurityConfiguration.java
+++ src/main/java/com/dvlk/SpringSecurityConfiguration.java
@@ -2,10 +2,8 @@ package com.dvlk;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,20 +20,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private static final Logger LOG = LogManager.getLogger();
 
-	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		SpringSecurityConfiguration.LOG.debug("Chargement des Utilisateurs locaux");
-		// Ne pas indiquer ROLE_, c'est ajoute automatiquement
-		final InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> memory = auth.inMemoryAuthentication();
-		memory.withUser("admin").password("admin").roles("ADMIN", "USER");
-		// Un autre
-		memory.withUser("guest").password("guest").roles("USER");
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		SpringSecurityConfiguration.LOG.debug("Chargement des regles de securisations");
-		http.authorizeRequests().antMatchers("/**").permitAll();
-		http.authorizeRequests().and().csrf().disable();
+		http.authorizeRequests().antMatchers("/salons").permitAll();
+		http.authorizeRequests().antMatchers("/profil/**").access("hasRole('ROLE_UTILISATEUR')");
+		http.authorizeRequests().anyRequest().authenticated();
 	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		jwtAuthenticationFilter resultat = new JwtAuthenticationFilter();
+	}
+	<beans:bean id="jwtAuthenticationFilter" class="com.toptal.travelplanner.security.JwtAuthenticationFilter">  (6)
+			<beans:property name="authenticationManager" ref="authenticationManager" />
+	<beans:property name="authenticationSuccessHandler" ref="jwtAuthenticationSuccessHandler" />  (7)
+	</beans:bean>
 }
